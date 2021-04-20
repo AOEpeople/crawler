711983c8c5d1a5ec4c97e36e77f6d1bfc421fd98
Nico de Haen <mail@ndh-websolutions.de>
2021-04-20 19:03:14 +0200

    [TASK] Support for PageTSConfig with processing instructions only

--- Classes/Controller/CrawlerController.php
+++ Classes/Controller/CrawlerController.php
@@ -605,13 +605,11 @@ class CrawlerController
         $pageTSconfig = $this->getPageTSconfigForId($id);
 
         $res = [];
-
         if (\is_array($pageTSconfig) && \is_array($pageTSconfig['tx_crawler.']['crawlerCfg.'])) {
             $crawlerCfg = $pageTSconfig['tx_crawler.']['crawlerCfg.'];
 
             if (\is_array($crawlerCfg['paramSets.'])) {
                 foreach ($crawlerCfg['paramSets.'] as $key => $values) {
-
                     $key = str_replace('.', '', $key);
                     // Sub configuration for a single configuration string:
                     $subCfg = (array)$crawlerCfg['paramSets.'][$key . '.'];
@@ -641,12 +639,42 @@ class CrawlerController
 
                             $res[$key]['subCfg'] = $subCfg;
                             $res[$key]['origin'] = 'pagets';
-
                             // recognize MP value
                             if (!$this->MP) {
                                 $res[$key]['URLs'] = $this->compileUrls($res[$key]['paramExpanded'], ['?id=' . $id]);
                             } else {
                                 $res[$key]['URLs'] = $this->compileUrls($res[$key]['paramExpanded'], ['?id=' . $id . '&MP=' . $this->MP]);
+                            }
+                        }
+                    } else if (!isset($res[str_replace('.', '', $key)])) {
+                        $key = str_replace('.', '', $key);
+                        // only processing instructions
+                        $subCfg = $values;
+                        $subCfg['key'] = $key;
+
+                        if (strcmp($subCfg['procInstrFilter'], '')) {
+                            $subCfg['procInstrFilter'] = implode(',', GeneralUtility::trimExplode(',', $subCfg['procInstrFilter']));
+                        }
+                        $pidOnlyList = implode(',', GeneralUtility::trimExplode(',', $subCfg['pidsOnly'], true));
+
+                        // process configuration if it is not page-specific or if the specific page is the current page:
+                        if (!strcmp($subCfg['pidsOnly'], '') || GeneralUtility::inList($pidOnlyList, $id)) {
+
+                                // add trailing slash if not present
+                            if (!empty($subCfg['baseUrl']) && substr($subCfg['baseUrl'], -1) != '/') {
+                                $subCfg['baseUrl'] .= '/';
+                            }
+
+                            // Explode, process etc.:
+                            $res[$key] = [];
+                            $res[$key]['subCfg'] = $subCfg;
+                            $res[$key]['paramParsed'] = [];
+                            $res[$key]['origin'] = 'pagets';
+                            // recognize MP value
+                            if (!$this->MP) {
+                                $res[$key]['URLs'] = ['?id=' . $id];
+                            } else {
+                                $res[$key]['URLs'] = ['?id=' . $id . '&MP=' . $this->MP];
                             }
                         }
                     }
@@ -927,7 +955,7 @@ class CrawlerController
                                 if ($subpartParams['_ENABLELANG'] && $transOrigPointerField) {
                                     $andWhereLanguage = ' AND ' . $this->db->quoteStr($transOrigPointerField, $subpartParams['_TABLE']) . ' <= 0 ';
                                 }
-								
+
                                 if($recursiveDepth > 0) {
                                     /** @var \TYPO3\CMS\Core\Database\QueryGenerator $queryGenerator */
                                     $queryGenerator = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\QueryGenerator::class);
